4dd4ed0483d390bd74d25276b588117eb3b16849
GalinaPimkina <133103137+GalinaPimkina@users.noreply.github.com>
2024-12-05 12:54:16 +0300

    add templates of models

--- grade_diary/diary/models.py
+++ grade_diary/diary/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Teacher(models.Model):
+    pass
+
+
+class ClassTeacher(models.Model):
+    pass
+
+
+class Class(models.Model):
+    pass
+
+
+class Student(models.Model):
+    pass
+
+
+class Grade(models.Model):
+    pass
