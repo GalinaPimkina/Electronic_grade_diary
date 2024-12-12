87434acd473c0e66390b8188d0cb791b0f1293e1
GalinaPimkina <133103137+GalinaPimkina@users.noreply.github.com>
2024-12-12 16:07:57 +0300

    edit models

--- grade_diary/diary/admin.py
+++ grade_diary/diary/admin.py
@@ -1,12 +1,5 @@
 from django.contrib import admin
-from .models import Teacher, Subject, Group, Student, Grade
-
-
-@admin.register(Teacher)
-class TeacherAdmin(admin.ModelAdmin):
-    list_display = ['name', 'subject', 'start_work_date']
-    list_display_links = ['name']
-    ordering = ['name', 'subject', 'start_work_date']
+from .models import Subject, Student, Grade
 
 
 @admin.register(Subject)
@@ -16,18 +9,11 @@ class SubjectAdmin(admin.ModelAdmin):
     ordering = ['subject']
 
 
-@admin.register(Group)
-class GroupAdmin(admin.ModelAdmin):
-    list_display = ['group_number', 'group_teacher']
-    list_display_links = ['group_number']
-    ordering = ['group_number']
-
-
 @admin.register(Student)
 class StudentAdmin(admin.ModelAdmin):
-    list_display = ['name', 'group', 'start_study_date']
+    list_display = ['name']
     list_display_links = ['name']
-    ordering = ['name', 'group', 'start_study_date']
+    ordering = ['name']
 
 
 @admin.register(Grade)
--- grade_diary/diary/models.py
+++ grade_diary/diary/models.py
@@ -1,17 +1,6 @@
 from django.db import models
 
 
-class Teacher(models.Model):
-    '''учителя'''
-
-    name = models.CharField(max_length=100, verbose_name='ФИО преподавателя')
-    subject = models.ForeignKey('Subject', on_delete=models.SET_NULL, null=True, verbose_name='Предмет', related_name='teacher')
-    start_work_date = models.DateTimeField(auto_now_add=True, verbose_name='Дата трудоустройства')
-
-    def __str__(self):
-        return f"{self.name}, предмет - {self.subject}"
-
-
 class Subject(models.Model):
     '''предметы'''
 
@@ -21,34 +10,30 @@ class Subject(models.Model):
         return self.subject
 
 
-class Group(models.Model):
-    '''классы'''
-
-    group_number = models.CharField(max_length=10, verbose_name='Номер группы')
-    group_teacher = models.ForeignKey('Teacher', on_delete=models.SET_NULL, null=True, verbose_name='Классный руководитель', related_name='group')
-
-    def __str__(self):
-        return f"{self.group_number}, классный руководитель - {self.group_teacher}"
-
-
 class Student(models.Model):
     '''учащиеся'''
 
     name = models.CharField(max_length=100, verbose_name='ФИО учащегося')
-    age = models.IntegerField(default=0, verbose_name='Возраст учащегося')
-    start_study_date = models.DateTimeField(auto_now_add=True, verbose_name='Дата поступления')
-    group = models.ForeignKey('Group', on_delete=models.SET_NULL, null=True, verbose_name='Класс', related_name='student')
+    grade = models.ForeignKey('Grade', on_delete=models.SET_NULL, null=True, verbose_name='Оценки', related_name='student')
 
     def __str__(self):
-        return f"{self.name}, класс - {self.group}"
+        return {self.name}
 
 
 class Grade(models.Model):
     '''оценки'''
 
-    grade = models.IntegerField(default=1, verbose_name='Оценка')
+    GRADE_CHOICES = [
+        (1, 1),
+        (2, 2),
+        (3, 3),
+        (4, 4),
+        (5, 5)
+    ]
+
+    grade = models.IntegerField(choices=GRADE_CHOICES, default=GRADE_CHOICES[0], verbose_name='Оценка')
     subject = models.ForeignKey('Subject', on_delete=models.SET_NULL, null=True, verbose_name='Предмет', related_name='grade')
-    student = models.ForeignKey('Student', on_delete=models.CASCADE, verbose_name='Учащийся', related_name='student')
 
     def __str__(self):
         return self.grade
+
