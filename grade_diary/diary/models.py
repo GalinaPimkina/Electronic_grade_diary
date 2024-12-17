25a7eba93868802b09f151d3eeea5a06e4aafe9f
GalinaPimkina <133103137+GalinaPimkina@users.noreply.github.com>
2024-12-17 17:43:58 +0300

    add new model studyroup, modified models student, teacher

--- grade_diary/diary/models.py
+++ grade_diary/diary/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Subject(models.Model):
@@ -10,10 +11,21 @@ class Subject(models.Model):
         return self.subject
 
 
-class Student(models.Model):
+class Teacher(User):
+    '''учителя'''
+
+    name = models.CharField(max_length=100, verbose_name='ФИО преподавателя')
+    subject = models.ForeignKey(Subject, on_delete=models.SET_NULL, null=True, verbose_name='Преподаватель', related_name='teacher')
+
+    def __str__(self):
+        return self.name
+
+
+class Student(User):
     '''учащиеся'''
 
     name = models.CharField(max_length=100, verbose_name='ФИО учащегося')
+    born_date = models.DateTimeField(verbose_name='Дата рождения')
 
     def __str__(self):
         return self.name
@@ -23,7 +35,13 @@ class Grade(models.Model):
     '''оценки'''
 
     grade = models.IntegerField(default=1, verbose_name='Оценка')
-    subject = models.ForeignKey('Subject', on_delete=models.SET_NULL, null=True, verbose_name='Предмет', related_name='grade')
-    student = models.ForeignKey('Student', on_delete=models.SET_NULL, null=True, verbose_name='Оценки', related_name='grade')
+    subject = models.ForeignKey(Subject, on_delete=models.SET_NULL, null=True, verbose_name='Предмет', related_name='grade')
+    student = models.ForeignKey(Student, on_delete=models.SET_NULL, null=True, verbose_name='Оценки', related_name='grade')
 
 
+class StudyGroup(models.Model):
+    '''учебная группа (класс)'''
+
+    number = models.CharField(max_length=10, verbose_name='Номер учебной группы')
+    student = models.ForeignKey(Student, on_delete=models.SET_NULL, null=True, verbose_name='Учащийся', related_name='study_group')
+    group_teacher = models.ForeignKey(Teacher, on_delete=models.SET_NULL, null=True, verbose_name='Классный руководитель', related_name='study_group')
--- grade_diary/grade_diary/settings.py
+++ grade_diary/grade_diary/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'diary',
-    'users',
 ]
 
 MIDDLEWARE = [
